65d94082006a61a7a2fc857be3d805459eb7bb0e
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-05-26 16:37:10 -0400

    initializes application, installs dependencies, removes global preset styles, creates Header and HeaderIcon components, finished Header section of the build

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+  );
+}
